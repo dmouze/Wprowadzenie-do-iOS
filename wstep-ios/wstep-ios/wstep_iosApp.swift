3ce1d59a22523173e2d4d5798115adc609489a9e
Mateusz <101819371+dmouze@users.noreply.github.com>
2023-10-29 09:20:17 +0100

    dmze
    przygotowanie repo

---
+++ wstep-ios/wstep-ios/wstep_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  wstep_iosApp.swift
+//  wstep-ios
+//
+//  Created by Matt on 29/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct wstep_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
